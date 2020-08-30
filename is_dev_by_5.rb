8022dad6c8496007116bdf71d822c3777b2acdbb
usamaeltmsah <usamaeltmsah@gmail.com>
2020-08-30 14:48:04 +0200

    Check if number divisible by another number

--- is_dev_by_5.rb
+++ is_dev_by_5.rb
@@ -1,8 +1,8 @@
-def is_div_by_5(number)
-	return number % 5 == 0
+def is_div_by(number, div)
+	return number % div == 0
 end
 
-puts "10 divisible by 5? " + is_div_by_5(10).to_s # => true
-puts "40 divisible by 5? " + is_div_by_5(40).to_s # => true
-puts "42 divisible by 5? " + is_div_by_5(42).to_s # => false
-puts "8 divisible by 5? " + is_div_by_5(8).to_s  # => false
+puts "10 divisible by 5? " + is_div_by(10, 5).to_s # => true
+puts "40 divisible by 6? " + is_div_by(40, 6).to_s # => true
+puts "42 divisible by 3? " + is_div_by(42, 3).to_s # => false
+puts "8 divisible by 2? " + is_div_by(8, 2).to_s  # => false
