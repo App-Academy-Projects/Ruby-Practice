6fbafa440e80b65e15514b1abb2bbaff487f004a
Usama Fouad <usamaeltmsah@gmail.com>
2020-08-30 14:59:14 +0200

    Which number is larger

--- larger_num.rb
+++ larger_num.rb
@@ -1,6 +1,9 @@
 def larger_number(num1, num2)
-	return num1 if num1 > num2 else num2
+	if num1 > num2
+		return num1
+	end
+	return num1
 end
 
-puts "Larger between 42 and 28 is: " + larger_number(42, 28)   # => 42
-puts "Larger between 99 and 100 is: " + larger_number(99, 100)  # => 100
+puts "Larger between 42 and 28 is: " + larger_number(42, 28).to_s   # => 42
+puts "Larger between 99 and 100 is: " + larger_number(99, 100).to_s  # => 100
