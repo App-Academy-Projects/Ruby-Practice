d5d6b52e4b4646063be53679dfc9053b314233ae
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-01 17:09:37 +0200

    Check if two strings characters are the same, no matter in same order

---
+++ Data Structure/Maps/0073.anagrams.rb
@@ -0,0 +1,16 @@
+def anagrams?(word1, word2)
+    if word1.length != word2.length
+      return false
+    end
+    word1.each_char do |ch|
+      if !word2.include?(ch)
+        return false
+      end
+    end
+    return true
+  end
+  
+  puts anagrams?("cat", "act")          #=> true
+  puts anagrams?("restful", "fluster")  #=> true
+  puts anagrams?("cat", "dog")          #=> false
+  puts anagrams?("boot", "bootcamp")    #=> false
