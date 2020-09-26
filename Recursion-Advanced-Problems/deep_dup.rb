0cf4f65c9feb637a8e62bd96f1f48b0aa5c23246
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-26 16:48:34 +0200

    Handle it to work for multi dimintional array

--- Recursion-Advanced-Problems/deep_dup.rb
+++ Recursion-Advanced-Problems/deep_dup.rb
@@ -1,20 +1,26 @@
 class Array
     def deep_dup
-        return self if self.is_a?(Array) && self.length <= 1
-
-        [self[0].dup] + self[1..-1].deep_dup
+        new_array = []
+        self.each do |el|
+            if el.is_a?(Array)
+                new_array << el.deep_dup
+            else
+                new_array << el
+            end
+        end
+        new_array
     end
 end
 
 robot_parts = [
   ["nuts", "bolts", "washers"],
-  ["capacitors", "resistors", "inductors"], "str"
+  [["capacitors", "resistors", "inductors"]], "str"
 ]
 
 robot_parts_copy = robot_parts.deep_dup
 
 # shouldn't modify robot_parts
-robot_parts_copy[1] << "LEDs"
+robot_parts_copy[1][0] << "LEDs"
 # but it does
 p robot_parts # => ["capacitors", "resistors", "inductors", "LEDs"]
 p robot_parts_copy
