41720f27190308a8e51ca0573a439cd01a26c794
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-26 12:51:39 +0200

    List range between start and end iteratively

--- Recursion-Advanced-Problems/range.rb
+++ Recursion-Advanced-Problems/range.rb
@@ -4,6 +4,23 @@ def range(my_start, my_end)
     [my_start] + range(my_start + 1, my_end)
 end
 
+puts "Recuirsively"
+p  range(1, 5)
+p  range(10, 50)
+p  range(-10, -20)
+p  range(-10, -2)
+
+def range(my_start, my_end)
+    range = []
+    while my_start <= my_end
+        range << my_start
+        my_start += 1
+    end
+    range
+end
+
+puts "------------------------------"
+puts "Iteratively"
 p  range(1, 5)
 p  range(10, 50)
 p  range(-10, -20)
